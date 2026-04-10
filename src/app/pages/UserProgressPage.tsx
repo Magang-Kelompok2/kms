1529cd91d4925f90729be2a67f549434e14c4558
vriska diah novita sari <vriskadiahnovitasari@gmail.com>
2026-04-10 16:29:58 +0700

    nambahin dikit

--- src/app/pages/UserProgressPage.tsx
+++ src/app/pages/UserProgressPage.tsx
@@ -207,7 +207,7 @@ export function UserProgressPage() {
 
         {/* PROGRESS */}
         <div className="mb-8">
-          <h2 className="text-2xl font-normal mb-4">
+          <h2 className="text-2xl font-bold mb-4">
             Progres Kelas
           </h2>
 
@@ -263,7 +263,7 @@ export function UserProgressPage() {
 
         {/* PENGUMPULAN */}
         <div>
-          <h2 className="text-2xl font-normal mb-4">Pengumpulan</h2>
+          <h2 className="text-2xl font-bold mb-4">Pengumpulan</h2>
 
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
             {userSubmissions.map((submission) => {
