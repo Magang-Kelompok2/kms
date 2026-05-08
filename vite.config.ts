786959755bee6bdebfd1c6f9645da84025314110
Muhammad Muflih Affandi <145008238+MuhammadAffandi24@users.noreply.github.com>
2026-05-08 17:08:19 +0700

    Ubah port agar tidak bentrok

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // Pastikan ini ada
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // Tambahkan ini di sini
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 3010, // Pakai 3001 supaya tidak bentrok dengan app-magang (3000) dan Backend (4000)
+    strictPort: true,
+    host: true,
+  },
+});
