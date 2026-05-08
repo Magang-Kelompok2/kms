ab11d39dc883dd2598965649860fe61234b4f334
Muhammad Muflih Affandi <145008238+MuhammadAffandi24@users.noreply.github.com>
2026-05-08 17:21:26 +0700

    Ubah port agar tidak bentrok

--- backend/src/lib/supabase.ts
+++ backend/src/lib/supabase.ts
@@ -1,8 +1,15 @@
 import { createClient } from "@supabase/supabase-js";
 import dotenv from "dotenv";
+import ws from "ws"; // 1. Tambahkan import ws
 dotenv.config();
 
 export const supabase = createClient(
   process.env.VITE_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
+  {
+    // 2. Beritahu Supabase untuk menggunakan library ws untuk fitur realtime
+    realtime: {
+      transport: ws as any,
+    },
+  },
 );
